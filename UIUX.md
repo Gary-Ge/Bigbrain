5d883288846db2b8ed849e2b55100aeab2e56e8a
Bohan Zhang <920060768@qq.com>
2023-04-17 23:34:11 +1000

    Add CheckTextField Test

---
+++ UIUX.md
@@ -0,0 +1,8 @@
+### 1. Always Visible Navbar
+The navigation bar is always at the top of the screen, no matter what scrolling you do on the screen. Make sure you can easily navigate to other pages (such as home page, join game page, etc.) without retyping the url or using the back key
+### 2. Autosave in Game Editing
+When editing the game, all the modifications you did would be auto saved after you unblurred that textfield/checkbox. You don't need to worry about losing your work by closing the page by mistake.
+### 3. Input Check and Prompt Text
+For most of the editable field, we provided input check and prompt text to tell you what kind of format/value/text is acceptable, make your editing more easily.
+### 4. Game Check
+When you want to finish editing or start a game, we will perform a game check to ensure that the game is valid. We will check if all the required field is finished. The game cannot be started if it is incompleted, preventing you from starting a half-finished game.
--- frontend/src/CheckTextField.test.jsx
+++ frontend/src/CheckTextField.test.jsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React, { useState } from 'react';
+import { mount } from 'enzyme';
 import CheckTextField from './Components/CheckTextField'
-import { TextField, InputAdornment } from '@mui/material';
+import { act } from 'react-dom/test-utils';
+import { TextField, InputAdornment, IconButton, Checkbox } from '@mui/material';
+
+function TestCheckTextField () {
+  const [disabled, setDisabled] = useState(false)
+  const [checked, setChecked] = useState(false)
+
+  const handleTextChange = (event) => {
+    if (event.target.value === null || event.target.value === '') {
+      setDisabled(true)
+      setChecked(false)
+    } else {
+      setDisabled(false)
+    }
+  }
+
+  const handleCheckedChange = (event) => {
+    setChecked(event.target.checked)
+  }
+
+  return (
+    <CheckTextField
+      label="Test Label"
+      required={true}
+      value="Test Value"
+      onChange={handleTextChange}
+      onBlur={() => {}}
+      name="Test Name"
+      onCheckBoxChange={handleCheckedChange}
+      checked={checked}
+      checkBoxDisabled={disabled}
+    >
+    </CheckTextField>
+  )
+}
 
 // CheckTextField is used to editing the choice of a question, which has a checkbox inside a text field.
 describe('CheckTextField', () => {
   let wrapper;
-  const onChange = jest.fn();
-  const onBlur = jest.fn();
-  const onCheckBoxChange = jest.fn();
+  const onChangeMock = jest.fn();
+  const onBlurMock = jest.fn();
+  const onCheckBoxChangeMock = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(
+    wrapper = mount(
       <CheckTextField
         label="Test Label"
         required={true}
         value="Test Value"
-        onChange={onChange}
-        onBlur={onBlur}
+        onChange={onChangeMock}
+        onBlur={onBlurMock}
         name="Test Name"
-        onCheckBoxChange={onCheckBoxChange}
+        onCheckBoxChange={onCheckBoxChangeMock}
         checked={false}
         checkBoxDisabled={false}
       />
@@ -29,19 +63,103 @@ describe('CheckTextField', () => {
   it('The amount of component and its sub-component should be correct', () => {
     expect(wrapper.find(TextField)).toHaveLength(1)
     expect(wrapper.find(InputAdornment)).toHaveLength(1)
+    expect(wrapper.find(IconButton)).toHaveLength(1)
+    expect(wrapper.find(Checkbox)).toHaveLength(1)
   })
 
-  it('Component should be rendered with indicated props', () => {
+  it('Normal Case: Component should be rendered with indicated props', () => {
     const textField = wrapper.find(TextField)
     expect(textField.prop('label')).toEqual('Test Label')
     expect(textField.prop('required')).toEqual(true)
     expect(textField.prop('value')).toEqual('Test Value')
-    expect(textField.prop('onChange')).toEqual(onChange)
-    expect(textField.prop('onBlur')).toEqual(onBlur)
+    expect(textField.prop('onChange')).toEqual(onChangeMock)
+    expect(textField.prop('onBlur')).toEqual(onBlurMock)
 
-    // const checkbox = wrapper.find(Checkbox)
-    // expect(checkbox.prop('name')).toEqual('Test Name')
-    // expect(checkbox.prop('checked')).toEqual(false)
-    // expect(checkbox.prop('disabled')).toEqual(false)
+    const checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('name')).toEqual('Test Name')
+    expect(checkbox.prop('checked')).toEqual(false)
+    expect(checkbox.prop('disabled')).toEqual(false)
+    expect(checkbox.prop('onChange')).toEqual(onCheckBoxChangeMock)
   })
-});
+
+  it('Edge Case: props should be their default value if they are not indicated', () => {
+    wrapper = mount(<CheckTextField />);
+    expect(wrapper.find(TextField).prop('required')).toEqual(false)
+    expect(wrapper.find(Checkbox).prop('checked')).toEqual(false)
+    expect(wrapper.find(Checkbox).prop('disabled')).toEqual(false)
+  })
+
+  it('Normal Case: Correspond function should be called when the text field has been changed, unblurred and the checkbox has been changed', () => {
+    const textField = wrapper.find(TextField)
+    act(() => {
+      textField.props().onChange({ target: { value: 'value' } })
+    })
+    expect(onChangeMock).toHaveBeenCalled()
+
+    act(() => {
+      textField.props().onBlur()
+    })
+    expect(onBlurMock).toHaveBeenCalled()
+
+    const checkbox = wrapper.find(Checkbox)
+    act(() => {
+      checkbox.props().onChange({ target: { checked: true } })
+    })
+    expect(onCheckBoxChangeMock).toHaveBeenCalled()
+  })
+
+  it('Function Test: The checkbox should be disabled if the textfield has no value, and vice versa', () => {
+    wrapper = mount(
+      <TestCheckTextField />
+    )
+    let checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('checked')).toEqual(false)
+    expect(checkbox.prop('disabled')).toEqual(false)
+
+    const textField = wrapper.find(TextField)
+    act(() => {
+      textField.props().onChange({ target: { value: '' } })
+    })
+    wrapper.update()
+
+    checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('checked')).toEqual(false)
+    expect(checkbox.prop('disabled')).toEqual(true)
+
+    act(() => {
+      textField.props().onChange({ target: { value: 'value' } })
+    })
+    wrapper.update()
+
+    checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('checked')).toEqual(false)
+    expect(checkbox.prop('disabled')).toEqual(false)
+  })
+
+  it('Function Test: Check and uncheck checkbox when checkbox is not disabled', () => {
+    wrapper = mount(
+      <TestCheckTextField />
+    )
+    let checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('checked')).toEqual(false)
+    expect(checkbox.prop('disabled')).toEqual(false)
+
+    act(() => {
+      checkbox.props().onChange({ target: { checked: true } })
+    })
+    wrapper.update()
+
+    checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('checked')).toEqual(true)
+    expect(checkbox.prop('disabled')).toEqual(false)
+
+    act(() => {
+      checkbox.props().onChange({ target: { checked: false } })
+    })
+    wrapper.update()
+
+    checkbox = wrapper.find(Checkbox)
+    expect(checkbox.prop('checked')).toEqual(false)
+    expect(checkbox.prop('disabled')).toEqual(false)
+  })
+})
